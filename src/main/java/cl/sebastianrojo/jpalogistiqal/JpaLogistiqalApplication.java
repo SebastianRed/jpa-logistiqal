0dd9fbe75b984611d8323a5570eaa1fcdddaf90e
Sebastián Rojo <sebastianrojocollao@gmail.com>
2021-11-10 21:41:51 -0300

    Commit inicial

---
+++ src/main/java/cl/sebastianrojo/jpalogistiqal/JpaLogistiqalApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.jpalogistiqal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLogistiqalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLogistiqalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/jpalogistiqal/JpaLogistiqalApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.jpalogistiqal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLogistiqalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
